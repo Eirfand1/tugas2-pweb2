c8cad02445a5bd88978768c84a1244ac8d03d56e
Eirfand1 <name.fandi07@proton.me>
2024-08-30 00:51:20 +0700

    add: index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+require_once './component/header.php';
+?>
+<body>
+   <?php require_once './component/navbar.php' ?>
+
+   <h1>Ini halam home ygy</h1>
+
+
+<?php
+
+require_once './component/footer.php';
+?>
