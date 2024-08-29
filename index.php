0d8cbe9204929c79cab8af26831e9466a4294465
Eirfand1 <name.fandi07@proton.me>
2024-08-30 00:53:12 +0700

    add: index.php

--- index.php
+++ index.php
@@ -2,9 +2,29 @@
 require_once './component/header.php';
 ?>
 <body>
-   <?php require_once './component/navbar.php' ?>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5 p-2">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="student">Data Mahasiswa</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="guidance">Data Bimbingan</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+
+<div>
 
    <h1>Ini halam home ygy</h1>
+   <h1></h1>(Agak negbug navbar disini)</h1>
+</div>
 
 
 <?php
